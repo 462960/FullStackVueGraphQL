b922b2b44423b59f7231494f9544783e334691ef
Volodymyr <v.maksymenko@sloboda-studio.com>
2018-10-18 17:36:36 +0300

    Got rid of basic example

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const todos = [
-  { task: "Wash", completed: false },
-  { task: "Sell", completed: false }
-];
-
 const typeDefs = gql`
   type Todo {
     task: String
@@ -14,29 +9,10 @@ const typeDefs = gql`
   type Query {
     todosList: [Todo]
   }
-
-  type Mutation {
-    addTodo(task: String, completed: Boolean): Todo
-  }
 `;
 
-const resolvers = {
-  Query: {
-    todosList: () => todos
-  },
-
-  Mutation: {
-    addTodo: (_, { task, completed }) => {
-      const todo = { task, completed };
-      todos.push(todo);
-      return todo;
-    }
-  }
-};
-
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  typeDefs
 });
 
 // By default server listening localhost:4000 but we can change to any by listen(any)
