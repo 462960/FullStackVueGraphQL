bd8346df2b12e4f371d2fe99e5ee5be86fe683ba
Volodymyr <v.maksymenko@sloboda-studio.com>
2018-10-19 14:03:34 +0300

    User/Post schema created

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
 
+const User = require("./models/User");
+const Post = require("./models/Post");
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("DB connected"))
   .catch(err => console.log(`Error ${err}`));
 
@@ -19,7 +25,11 @@ const typeDefs = gql`
 `;
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
 });
 
 // By default server listening localhost:4000 but we can change to any by listen(any)
