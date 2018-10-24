1b30df683218091ece21683a2dc55621f1582aad
Volodymyr <v.maksymenko@sloboda-studio.com>
2018-10-24 15:08:17 +0300

    Vuetify plugin is added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <v-app>
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
     <h1>App</h1>
     <router-view />
-  </div>
+  </v-app>
 </template>
 
 
---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify, {
+  iconfont: "md",
+  theme: {
+    primary: "#CFD8DC",
+    secondary: "#90A4AE",
+    accent: "#A1887F",
+    error: "#F44336",
+    warning: "#FBC02D",
+    info: "#2196f3",
+    success: "#4caf50"
+  }
+});
