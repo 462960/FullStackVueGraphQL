c61af0da54827aeccd36fc61009cbd155a79fb20
Volodymyr <v.maksymenko@sloboda-studio.com>
2018-10-29 10:08:49 +0200

    Horisontal NavBar and MobileFirst design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,49 @@
 <template>
   <v-app>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <h1>App</h1>
-    <router-view />
+    <!-- Horisontal navbar -->
+    <v-toolbar fixed color="primary" dark>
+      <!-- App title -->
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer">
+          VueShare
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- Search input -->
+      <v-text-field flex prepend-icon="search" placeholder="Search posts" color="accent" single-line hide-details></v-text-field>
+      <v-spacer></v-spacer>
+      <!-- Horisontal navbar links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in horisontalNavItems" :key="item.title" :to="item.link">
+          <v-icon left class="hidden-sm-only">{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <!-- App content -->
+    <main>
+      <v-container class="mt-4">
+        <router-view />
+      </v-container>
+    </main>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    horisontalNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  }
+};
+</script>
+
 
 
