0bae51d40204cf3b4980341ea05c6e74e5956c0a
Volodymyr <v.maksymenko@sloboda-studio.com>
2018-10-29 11:53:52 +0200

    SidebarNav added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <v-app>
+  <v-app style="background: #E3E3EE">
+
+    <!-- Side navbar -->
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <!-- Side navbar links -->
+      <v-list>
+        <v-list-tile ripple v-for="item in sideNavItems" :key="item.title" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <!-- Horisontal navbar -->
     <v-toolbar fixed color="primary" dark>
       <!-- App title -->
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer">
           VueShare
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
       <!-- Search input -->
       <v-text-field flex prepend-icon="search" placeholder="Search posts" color="accent" single-line hide-details></v-text-field>
       <v-spacer></v-spacer>
+
       <!-- Horisontal navbar links -->
       <v-toolbar-items class="hidden-xs-only">
         <v-btn flat v-for="item in horisontalNavItems" :key="item.title" :to="item.link">
@@ -21,6 +47,7 @@
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
     <!-- App content -->
     <main>
       <v-container class="mt-4">
@@ -33,6 +60,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horisontalNavItems() {
       return [
@@ -40,6 +72,18 @@ export default {
         { icon: "lock_open", title: "Sign In", link: "/signin" },
         { icon: "create", title: "Sign Up", link: "/signup" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };
